72a008234144ad70ddfc9258086e2f7ddff6c71e
Oumayma <oumayma.cherif@esprit.tn>
2024-11-10 22:15:40 +0100

    test

--- Backend/src/main/java/tn/esprit/spring/configs/WebConfig.java
+++ Backend/src/main/java/tn/esprit/spring/configs/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")  // Adresse de ton frontend Angular
+                .allowedOrigins("http://192.168.64.20")  // Adresse de ton frontend Angular
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
