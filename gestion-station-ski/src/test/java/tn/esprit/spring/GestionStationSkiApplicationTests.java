bbef1a1bcd526dc36950f1ad4a3521b69aab221b
amal souli <91203142+amalsouli@users.noreply.github.com>
2024-11-11 09:26:37 +0100

    Update GestionStationSkiApplicationTests.java

--- gestion-station-ski/src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
+++ gestion-station-ski/src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
@@ -2,12 +2,13 @@ package tn.esprit.spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application.properties")
 class GestionStationSkiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
